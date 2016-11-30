adbd7042632e583df3297f2d04ec439bcb9626c1
smileztv <zaur.tv@gmail.com>
2016-12-01 00:47:21 +0600

    change_pom

--- chapter_001/src/main/java/task1_4_3/Paint.java
+++ chapter_001/src/main/java/task1_4_3/Paint.java
@@ -9,7 +9,7 @@ public class Paint {
     public String piramid(int h) {
         StringBuilder sbuilder = new StringBuilder();
         for (int i = 0; i < h; i++) {
-             //sbuilder.append("\n");
+            //sbuilder.append("\n");
             int a = (h - 1) - i;
             for (int b = a; b > 0; b--) {
                 sbuilder.append(" ");
